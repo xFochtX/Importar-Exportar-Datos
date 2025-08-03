469f50762be152994e9ff4df5d27ab9bc8b03b58
Fabian Oswaldo Chipana Ticona <fabian.chipana@minsur.com>
2025-08-02 19:44:45 -0500

    fix: version setup

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="ImportarExportarDatos",
-    version="1.0.0",
+    version="1.1.0",
     packages=find_packages(),
     install_requires=[
         "pandas",
         "openpyxl"
     ],
     author="Focht",
-    description="Librería para importar y exportar datos en Excel y pickle",
+    description="Librería para importar y exportar datos en excel, csv y pickle",
     author_email="fabian.chipana@unmsm.edu.pe",
     url="https://github.com/xFochtX/Importar-Exportar-Datos.git",
 )
