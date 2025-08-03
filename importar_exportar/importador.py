da092166cacc967b16deeed1cc384f496387efb5
Fabian Oswaldo Chipana Ticona <fabian.chipana@minsur.com>
2025-08-02 22:17:34 -0500

    feat: add PDF method into Importador class

--- importar_exportar/importador.py
+++ importar_exportar/importador.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import pickle
+import pdfplumber
 from .base import ArchivoBase
 
 class Importador(ArchivoBase):
@@ -12,3 +13,35 @@ class Importador(ArchivoBase):
 
   def csv(self,**kwargs):
     return pd.read_csv(self.ruta_archivo, **kwargs)
+
+  def pdf(self, type='table', n_pages='all', table_settings=None):
+    """
+    Extrae tablas o texto desde un archivo PDF usando pdfplumber.
+
+    Parámetros:
+    - type: 'table' para tablas o 'text' para texto plano
+    - n_pages: 'all' o lista de páginas (base 1)
+    - table_settings: configuraciones opcionales para detección de tablas
+
+    Retorna:
+    - Si type='table': lista de DataFrames, uno por cada tabla encontrada
+    - Si type='text' : lista de strings, uno por cada página
+    """
+    information = []
+
+    with pdfplumber.open(self.ruta_archivo) as pdf:
+      pages = pdf.pages if n_pages == 'all' else [pdf.pages[p - 1] for p in n_pages]
+
+      for page in pages:
+        if type == 'table':
+          extracted = page.extract_tables(table_settings=table_settings)
+          for tabla in extracted:
+            df = pd.DataFrame(tabla[1:], columns=tabla[0])
+            information.append(df)
+        elif type == 'text':
+          text = page.extract_text()
+          information.append(text)
+        else:
+          raise ValueError("El parámetro 'type' debe ser 'table' o 'text'.")
+
+    return information
